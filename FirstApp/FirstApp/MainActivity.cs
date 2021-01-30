a88b41ff6a953ca3c745f667290e258e81fa4404
Helen Lants <helen.lants@proekspert.ee>
2021-01-30 14:01:56 +0200

    Added calculator layout

--- FirstApp/FirstApp/MainActivity.cs
+++ FirstApp/FirstApp/MainActivity.cs
@@ -15,6 +15,20 @@ namespace FirstApp
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var textView = FindViewById<TextView>(Resource.Id.textView1);
+            var button = FindViewById<Button>(Resource.Id.button1);
+            var calculatorButton = FindViewById<Button>(Resource.Id.calculatorButton);
+            
+            button.Click += delegate
+            {
+                textView.Text = "Hello World!";
+            };
+            int count = 1;
+            button.Click += delegate {
+                button.Text = string.Format("{0} clicks!", count++);
+            };
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
