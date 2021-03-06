af26ecabbf62b7e530ef47cfd016edf2b4929583
Helen Lants <helen.lants@proekspert.ee>
2021-03-06 17:03:32 +0200

    Added CarAdapter

--- FirstApp/FirstApp/SampleListActivity.cs
+++ FirstApp/FirstApp/SampleListActivity.cs
@@ -4,6 +4,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using FirstApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +15,28 @@ namespace FirstApp
     [Activity(Label = "SampleListActivity")]
     public class SampleListActivity : Activity //muutsime tüübi ListActivityks alguses ja siis tagasi Activityks, et saaks disaini kasutada. activity ei tea midagi listidest, seega ei saa ka adapterit kasutada ja tuleb disaini fail teha
     {
-        string[] items;
+        List<Car> items;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.simplelist_layout);
 
             var listView = FindViewById<ListView>(Resource.Id.listView1);
-            items = new string[] {"Volkswagen", "BMW", "Fiat" };
-            listView.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, items); //tuli kaasa anda activity-> this, disain, ja see asjade list ehk items
+            items = new List<Car> 
+            { 
+                new Car {Manufacturer="Ford", Model = "Focus", KW =100 },
+                new Car {Manufacturer="Volkswagen", Model = "Passat", KW =45 },
+                new Car {Manufacturer="BMW", Model = "X5", KW =100 },
+                new Car {Manufacturer="Audi", Model = "A7", KW =3 },
+                new Car {Manufacturer="BMW", Model = "X7", KW =787 },
+                new Car {Manufacturer="BMW", Model = "6", KW =55 },
+                new Car {Manufacturer="Ford2", Model = "Focus3", KW =5 },
+                new Car {Manufacturer="Ford3", Model = "Focus6", KW =77 },
+                new Car {Manufacturer="Ford4", Model = "Focus8", KW =99 }
+            };
+
+            listView.Adapter = new CarAdapter(this, items); //tuli kaasa anda activity-> this, disain, ja see asjade list ehk items
+
 
             // Create your application here
         }
