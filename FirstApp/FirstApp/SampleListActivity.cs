2dda80a9473b1b185de5c6b0969e367c935d8c47
Helen Lants <helen.lants@proekspert.ee>
2021-03-06 17:12:59 +0200

    added cars

--- FirstApp/FirstApp/SampleListActivity.cs
+++ FirstApp/FirstApp/SampleListActivity.cs
@@ -32,6 +32,15 @@ namespace FirstApp
                 new Car {Manufacturer="BMW", Model = "6", KW =55 },
                 new Car {Manufacturer="Ford2", Model = "Focus3", KW =5 },
                 new Car {Manufacturer="Ford3", Model = "Focus6", KW =77 },
+                new Car {Manufacturer="Ford4", Model = "Focus8", KW =99 },
+                new Car {Manufacturer="Ford", Model = "Focus", KW =100 },
+                new Car {Manufacturer="Volkswagen", Model = "Passat", KW =45 },
+                new Car {Manufacturer="BMW", Model = "X5", KW =100 },
+                new Car {Manufacturer="Audi", Model = "A7", KW =3 },
+                new Car {Manufacturer="BMW", Model = "X7", KW =787 },
+                new Car {Manufacturer="BMW", Model = "6", KW =55 },
+                new Car {Manufacturer="Ford2", Model = "Focus3", KW =5 },
+                new Car {Manufacturer="Ford3", Model = "Focus6", KW =77 },
                 new Car {Manufacturer="Ford4", Model = "Focus8", KW =99 }
             };
 
