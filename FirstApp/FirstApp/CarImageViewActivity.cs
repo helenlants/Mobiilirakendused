db2b437e34bd7b8d081c685d66260b2eb012afaf
Helen Lants <helen.lants@proekspert.ee>
2021-04-02 20:39:22 +0300

    WeatherApp added

---
+++ FirstApp/FirstApp/CarImageViewActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+namespace FirstApp
+{
+    WebView _webView;
+    [Activity(Label = "CarImageViewActivity")]
+    public class CarImageViewActivity : Activity
+    {
+        base.OnCreate(savedInstanceState);
+        SetContentView(Resource.Layout.);
+
+        // Create your application here
+        var address = Intent.GetStringExtra(Constants.AddressKey);
+        _webView = FindViewById<WebView>(Resource.Id.webView1);
+            _webView.Settings.JavaScriptEnabled = true;
+            _webView.SetWebViewClient(new SimpleWebViewClient());
+            _webView.LoadUrl(address);
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}*/
---
+++ WeatherApp/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Net.Http;
+
+namespace WeatherApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var apiKey = "b051ac84c077d3e777344e7ac8b5af54";
+
+            var client = new HttpClient();
+            var response = await client.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?q=tallinn&appid={apiKey}");
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
